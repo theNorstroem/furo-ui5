82c4c329b563b820e312be97c8ae19be48224be2
veith <veithz@gmail.com>
2022-04-05 08:51:51 +0200

    fix: invalid subfield binding (remove it)

--- src/typerenderer/form-string.js
+++ src/typerenderer/form-string.js
@@ -4,10 +4,6 @@ import { FuroUi5TextInputLabeled } from '../furo-ui5-text-input-labeled.js';
  * @summary form renderer for `string`
  * @element form-string
  */
-export class FormString extends FuroUi5TextInputLabeled {
-  bindData(fieldNode) {
-    super.bindData(fieldNode.data);
-  }
-}
+export class FormString extends FuroUi5TextInputLabeled {}
 
 window.customElements.define('form-string', FormString);
