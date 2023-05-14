d7551af845919ad873f296dd29cb6fc3d7af3139
veith <veithz@gmail.com>
2023-05-14 09:20:02 +0200

    feat: web-types packages for ui5 and ui5-fiori

--- scripts/webtypes.sh
+++ scripts/webtypes.sh
@@ -7,3 +7,10 @@ cwd=$(pwd)
 npm run analyze
 jq '. + input' package.json custom-elements.json | simple-generator -t ./scripts/templates/webtypes.tpl > web-types.json
 echo "✅ web-types.json"
+
+jq '. + input' ./node_modules/@ui5/webcomponents-fiori/package.json ./node_modules/@ui5/webcomponents-fiori/dist/custom-elements.json | simple-generator -t ./scripts/templates/webtypes_cem1.tpl > ./external-web-types/@ui5/webcomponents-fiori/web-types.json
+echo "✅ external-web-types/@ui5/webcomponents-fiori/web-types.json"
+
+
+jq '. + input' ./node_modules/@ui5/webcomponents/package.json ./node_modules/@ui5/webcomponents/dist/custom-elements.json | simple-generator -t ./scripts/templates/webtypes_cem1.tpl > ./external-web-types/@ui5/webcomponents/web-types.json
+echo "✅ external-web-types/@ui5/webcomponents/web-types.json"
